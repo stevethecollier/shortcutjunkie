ad1df01f9dd7c56cf7b80fc6b41c556ff070efc7
Juan Caicedo <juan.caicedo@alumni.ubc.ca>
2014-09-20 11:55:09 -0400

    hooked up shortcut controller to module

--- public/js/controllers/ShortcutController.js
+++ public/js/controllers/ShortcutController.js
@@ -1,26 +1,28 @@
-function ShortcutController($scope, $http, $timeout) {
-	console.log("Made it into the ShortcutController")
-	$scope.shortcuts = [];
-	$scope.newShortcut = {
-		application : "",
-		keyset : "",
-		description : ""
-	};
+angular.module("sj").controller("shortcutController", ['$scope', '$http', '$timeout',
+	function ShortcutController($scope, $http, $timeout) {
+		console.log("Made it into the ShortcutController");
+		$scope.shortcuts = [];
+		$scope.newShortcut = {
+			application : "",
+			keyset : "",
+			description : ""
+		};
 
-	$scope.setShortcuts = function(shortcuts) {
-		$scope.shortcuts = shortcuts;
-	};
+		$scope.setShortcuts = function(shortcuts) {
+			$scope.shortcuts = shortcuts;
+		};
 
-	$scope.addNewShortcut = function() {
-		$http.post('/shortcut.json', $scope.newShortcut).success(function(data) {
-			if (data.shortcut) {
-				$scope.shortcuts.push(data.shortcut);
-				//$scope.newTodo.description = '';
-			} else {
-				alert(JSON.stringify(data));
-			}
-		});
-	};
+		$scope.addNewShortcut = function() {
+			$http.post('/shortcut.json', $scope.newShortcut).success(function(data) {
+				if (data.shortcut) {
+					$scope.shortcuts.push(data.shortcut);
+					//$scope.newTodo.description = '';
+				} else {
+					alert(JSON.stringify(data));
+				}
+			});
+		};
+}]);
 
 	// $scope.update = function(todo) {
 	// 	$http.put('/todo/' + todo._id + '.json', todo).success(function(data) {
@@ -45,5 +47,3 @@ function ShortcutController($scope, $http, $timeout) {
 	// }, 30 * 60 * 1000); // update every 30 minutes;
 	//
 	// $scope.updateList();
-
-}
