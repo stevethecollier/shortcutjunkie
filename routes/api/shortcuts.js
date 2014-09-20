4691f4b65360fa641243dc50f316aa0156b634ab
Steven Collier <jedi.ninja92@gmail.com>
2014-09-20 11:13:29 -0400

    shortcuts api

--- routes/api/shortcuts.js
+++ routes/api/shortcuts.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-var shortcuts = require('./shortcuts');
-
 /* GET users listing. */
-router.use('/shortcuts', shortcuts);
+router.get('/', function(req, res) {
+    res.send('respond with a resource');
+});
 
 module.exports = router;
