ffca87155b6bdd8fb615b96d0cf58bfc8bc2a457
Juan Caicedo <juan.caicedo@alumni.ubc.ca>
2014-10-03 15:01:57 -0400

    added bootstrap navbar

--- public/js/app.js
+++ public/js/app.js
@@ -1,23 +1,32 @@
-angular.module('sj', ['ui.router', 'ngSanitize'])
+angular.module('sj', ['ui.router', 'ngSanitize', 'ui.bootstrap'])
     .config(['$stateProvider', '$urlRouterProvider',
     	function($stateProvider, $urlRouterProvider){
 	    	$urlRouterProvider.otherwise('/');
 	    	$stateProvider.
 	    		state('home', {
-		    		url : "/",
-		    		templateUrl : "/partials/home.html"
+		    		url : '/',
+		    		templateUrl : '/partials/home.html',
+		    		data:{
+		    			title : 'Home'
+		    		}
 		    	});
 	        $stateProvider
 	            .state('shortcuts', {
-	                url : "/partials/shortcuts", 
-	                templateUrl: "/partials/shortcutHome.html",
-	                controller:"shortcutController"
+	                url : '/partials/shortcuts', 
+	                templateUrl: '/partials/shortcutHome.html',
+	                controller:'shortcutController',
+	                data:{
+	                	title : 'Shortcuts Home'
+	                }
 	            });
 	        $stateProvider
 	        	.state('search', {
-	        		url : "/partials/search",
-	        		templateUrl : "/partials/search.html",
-	        		controller:"searchController"
+	        		url : '/partials/search',
+	        		templateUrl : '/partials/search.html',
+	        		controller:'searchController',
+	        		data:{
+	        			title : 'Search'
+	        		}
 	        	});
     }
 ]);
