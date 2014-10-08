a31a2a4173391fca6fbdced61cc9cef34eef6d97
Juan Caicedo <juan.caicedo@alumni.ubc.ca>
2014-10-08 16:11:35 -0400

    started test for editing

--- tests/fullTest.js
+++ tests/fullTest.js
@@ -1,6 +1,6 @@
 exports.config = {
     seleniumAddress: 'http://localhost:4444/wd/hub',
     specs: ['spec/navigation.js',
-        'spec/addShortcut.js'
+        'spec/addAndDeleteShortcut.js'
     ]
 }
--- tests/spec/shortcuts.js
+++ tests/spec/shortcuts.js
@@ -1,32 +1,46 @@
-describe('shortcut page', function() {
-	it('should have a title', function() {
-		browser.get('http://localhost:3000/#/partials/shortcuts');
-		expect(browser.getTitle()).toEqual('Shortcuts Home');
-	});
+describe('shortcuts', function() {
+    it('should have a home', function() {
+        browser.get('http://localhost:3000/#/partials/shortcuts');
+        expect(browser.getTitle()).toEqual('Shortcuts Home');
+    });
 
-	it('should add a shortcut', function() {
-		element.all(by.css('.shortcut')).then(function(elems) {
-			var originalCount = elems.length;
+    it('should add a shortcut', function() {
+        element.all(by.css('.shortcut')).then(function(elements) {
+            var originalCount = elements.length;
 
-			element(by.id('applicationField')).sendKeys('testApplication');
-			element(by.id('operatingSystemField')).sendKeys('testOperatingSystem');
-			element(by.id('keysetField')).sendKeys('testKeyset');
-			element(by.id('descriptionField')).sendKeys('testDescription');
-			element(by.id('submitNew')).click();
+            element(by.id('applicationField')).sendKeys('testApplication');
+            element(by.id('operatingSystemField')).sendKeys('testOperatingSystem');
+            element(by.id('keysetField')).sendKeys('testKeyset');
+            element(by.id('descriptionField')).sendKeys('testDescription');
+            element(by.id('submitNew')).click();
 
-			var newCount = element.all(by.css('.shortcut')).count();
-			expect(newCount).toEqual(originalCount + 1);
-		});
-	});
+            var newCount = element.all(by.css('.shortcut')).count();
+            expect(newCount).toEqual(originalCount + 1);
+        });
+    });
 
-	it('should delete a shortcut', function() {
-		element.all(by.css('.shortcut')).then(function(elems) {
-			var originalCount = elems.length;
+    it('should edit a shortcut', function() {
+        element.all(by.css('.shortcut')).then(function(elements) {
+            var shortcut = elements[elements.length - 1];
+            shortcut.element(by.css('.idInfo')).getText().then(function(text) {
+                //go to edit page
+                var idIndex = text.indexOf('=') + 2;
+                browser.get('http://localhost:3000/#/partials/edit/' + text.substr(idIndex));
+                expect(browser.getTitle()).toEqual('Edit Shortcut');
+                //edit 
+            });
+        });
+    });
 
-			element.all(by.css('.deleteLink')).last().click();
+    it('should delete a shortcut', function() {
+        browser.get('http://localhost:3000/#/partials/shortcuts');
+        element.all(by.css('.shortcut')).then(function(elements) {
+            var originalCount = elements.length;
 
-			var newCount = element.all(by.css('.shortcut')).count();
-			expect(newCount).toEqual(originalCount - 1);
-		});
-	});
+            element.all(by.css('.deleteLink')).last().click();
+
+            var newCount = element.all(by.css('.shortcut')).count();
+            expect(newCount).toEqual(originalCount - 1);
+        });
+    });
 });
