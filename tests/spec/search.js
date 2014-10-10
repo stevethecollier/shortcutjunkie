eca3cdf736155c4da862b4009303a31f8a6a8d9a
Juan Caicedo <juan.caicedo@alumni.ubc.ca>
2014-10-10 14:40:00 -0400

    Improved testing code structure

--- tests/spec/search.js
+++ tests/spec/search.js
@@ -5,34 +5,42 @@ var testShortcut = {
     description: 'testDescription'
 }
 
-var indexes = [];
-
 describe('search page', function() {
-    it('should find a shortcut by all fields', function() {
+    var indexes = [];
+    beforeEach(function() {
         addTestShortcut();
         browser.get('http://localhost:3000/#/partials/search');
-        //esablish indexes
+        indexes = establishIndexes();
+    });
 
-        element(by.id('searchType')).all(by.tagName('option'))
-            .then(function(options) {
-                for (var element in options) {
-                    options[element].getText().then(function(value) {
-                        indexes.push(value);
-                    });
-                }
-            });
-
+    it('should find a shortcut by all fields', function() {
         for (var property in testShortcut) {
             element(by.id('searchCriteria')).clear().sendKeys(testShortcut[property]);
-            selectDropdownByNum(element(by.id('searchType')), property);
+            selectDropdownByNum(element(by.id('searchType')), property, indexes);
             element(by.id('search')).click();
             var results = element.all(by.css('.shortcut')).count();
             expect(results).toBeGreaterThan(0);
         }
+    });
 
+    afterEach(function() {
         deleteLastShortcut();
     });
 });
+
+//helper functions
+function establishIndexes() {
+    var indexes = [];
+    element(by.id('searchType')).all(by.tagName('option'))
+        .then(function(options) {
+            for (var element in options) {
+                options[element].getText().then(function(value) {
+                    indexes.push(value);
+                });
+            }
+        });
+    return indexes;
+}
 
 function addTestShortcut() {
     browser.get('http://localhost:3000/#/partials/shortcuts');
@@ -49,7 +57,7 @@ function deleteLastShortcut() {
     element.all(by.css('.deleteLink')).last().click();
 }
 
-function selectDropdownByNum(element, searchType) {
+function selectDropdownByNum(element, searchType, indexes) {
     if (searchType) {
         element.all(by.tagName('option'))
             .then(function(options) {
