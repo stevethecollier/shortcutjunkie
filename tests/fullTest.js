76713f67db3d5448d43642354ec3a76a7e4a35e9
Juan Caicedo <juan.caicedo@alumni.ubc.ca>
2014-10-10 13:33:08 -0400

    basic search test

--- tests/fullTest.js
+++ tests/fullTest.js
@@ -1,6 +1,7 @@
 exports.config = {
     seleniumAddress: 'http://localhost:4444/wd/hub',
     specs: ['spec/navigation.js',
-        'spec/shortcut.js'
+        'spec/shortcut.js',
+        'spec/search.js'
     ]
 }
