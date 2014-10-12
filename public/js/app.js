8146d8357b7d90f1f092a43ac499e822b782918b
Juan Caicedo <juan.caicedo@alumni.ubc.ca>
2014-10-12 12:54:31 -0400

    added redirect for edit with no param

--- public/js/app.js
+++ public/js/app.js
@@ -1,41 +1,49 @@
 angular.module('sj', ['ui.router', 'ngSanitize', 'ui.bootstrap', 'ngRoute'])
-	.config(['$stateProvider', '$urlRouterProvider',
-		function($stateProvider, $urlRouterProvider){
-			$urlRouterProvider.otherwise('/');
-			$stateProvider.
-				state('home', {
-					url : '/',
-					templateUrl : '/partials/home.html',
-					data:{
-						pageTitle : 'Shortcut Junkie'
-					}
-				});
-			$stateProvider
-				.state('shortcuts', {
-					url : '/partials/shortcuts', 
-					templateUrl: '/partials/shortcutHome.html',
-					controller:'shortcutController',
-					data:{
-						pageTitle : 'Shortcuts Home'
-					}
-				});
-			$stateProvider
-				.state('search', {
-					url : '/partials/search',
-					templateUrl : '/partials/search.html',
-					controller:'searchController',
-					data:{
-						pageTitle : 'Search Shortcuts'
-					}
-				});
-			$stateProvider
-				.state('edit', {
-					url : '/partials/edit/{id}',
-					templateUrl : '/partials/editShortcut.html',
-					controller:'editController',
-					data:{
-						pageTitle : 'Edit Shortcut'
-					}
-				});
-	}
-]);
+    .config(['$stateProvider', '$urlRouterProvider',
+        function($stateProvider, $urlRouterProvider) {
+            $urlRouterProvider.otherwise('/');
+            $stateProvider.
+                state('home', {
+                    url: '/',
+                    templateUrl: '/partials/home.html',
+                    data: {
+                        pageTitle: 'Shortcut Junkie'
+                    }
+                });
+            $stateProvider
+                .state('shortcuts', {
+                    url: '/partials/shortcuts',
+                    templateUrl: '/partials/shortcutHome.html',
+                    controller: 'shortcutController',
+                    data: {
+                        pageTitle: 'Shortcuts Home'
+                    }
+                });
+            $stateProvider
+                .state('search', {
+                    url: '/partials/search',
+                    templateUrl: '/partials/search.html',
+                    controller: 'searchController',
+                    data: {
+                        pageTitle: 'Search Shortcuts'
+                    }
+                });
+            //Edit state is only valid if there is an id
+            $stateProvider
+                .state('editError', {
+                    url: '/partials/edit/',
+                    onEnter: function($state) {
+                        $state.go('home');
+                    },
+                });
+            $stateProvider
+                .state('edit', {
+                    url: '/partials/edit/{id}',
+                    templateUrl: '/partials/editShortcut.html',
+                    controller: 'editController',
+                    data: {
+                        pageTitle: 'Edit Shortcut'
+                    }
+                });
+        }
+    ]);
