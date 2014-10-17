67639e6a2deeb813c180e607a67e606706b0c353
Juan Caicedo <juan.caicedo@alumni.ubc.ca>
2014-10-17 15:34:13 -0400

    cleaned up validation

--- public/js/controllers/ShortcutController.js
+++ public/js/controllers/ShortcutController.js
@@ -5,8 +5,6 @@ angular.module('sj').controller('shortcutController', ['$scope', '$http', '$time
             $scope.foundShortcuts = data.shortcuts;
         })
 
-        $scope.newShortcut = {};
-
         $scope.delete = function(shortcut) {
             $http({
                 method: 'DELETE',
@@ -39,11 +37,18 @@ angular.module('sj').controller('shortcutController', ['$scope', '$http', '$time
             $scope.foundShortcuts = shortcuts;
         };
 
+        $scope.newShortcut = {};
+        
         $scope.addNewShortcut = function(shortcutForm) {
+            $scope.submitted = true;
             if (shortcutForm.$valid) {
                 $http.post('/api/shortcuts', $scope.newShortcut).success(function(data) {
                     if (data) {
                         $scope.foundShortcuts.push(data);
+                        //reset form
+                        $scope.newShortcut = {};
+                        $scope.shortcutForm.$setPristine;
+                        $scope.submitted = false;
                     } else {
                         alert(JSON.stringify(data));
                     }
