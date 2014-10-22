bf60df1c25a16c68870e5fa86522906a2ee9b59e
Juan Caicedo <juan.caicedo@alumni.ubc.ca>
2014-10-22 15:55:50 -0400

    alternating sign in and sign out buttons

--- public/js/controllers/LoginController.js
+++ public/js/controllers/LoginController.js
@@ -1,17 +1,34 @@
 var sjModule = angular.module('sj');
 
 sjModule.controller('loginController', ['$scope', 'auth', '$location', 'store',
-    function LoginCtrl($scope, auth, $location, store ) {
-        $scope.message = 'message';
+    function LoginCtrl($scope, auth, $location, store) {
+        $scope.loggedIn = false;
+
+        $scope.execute = function() {
+            if ($scope.loggedIn){
+                $scope.logout();
+            } else {
+                $scope.login();
+            }
+        }
+
         $scope.login = function() {
             auth.signin({}, function(profile, token) {
                 // Success callback
+                $scope.loggedIn = true;
                 store.set('profile', profile);
                 store.set('token', token);
                 $location.path('/');
             }, function() {
                 // Error callback
             });
+        }
+
+        $scope.logout = function() {
+            $scope.loggedIn= false;
+            auth.signout();
+            store.remove('profile');
+            store.remove('token');
         }
     }
 ]);
