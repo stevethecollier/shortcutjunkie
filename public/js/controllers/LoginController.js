6d04b6cb639f38e543af6f41d059d89fbf9a9afc
Juan Caicedo <juan.caicedo@alumni.ubc.ca>
2014-10-23 17:20:01 -0400

    buttons to sign in and out

--- public/js/controllers/LoginController.js
+++ public/js/controllers/LoginController.js
@@ -4,6 +4,8 @@ sjModule.controller('loginController', ['$scope', 'auth', '$location', 'store',
     function LoginCtrl($scope, auth, $location, store) {
         $scope.loggedIn = false;
 
+        $scope.message = 'message';
+
         $scope.execute = function() {
             if ($scope.loggedIn) {
                 $scope.logout();
@@ -15,15 +17,15 @@ sjModule.controller('loginController', ['$scope', 'auth', '$location', 'store',
         $scope.login = function() {
             auth.signin({}, function(profile, token) {
                 // Success callback
-                debugger;
                 store.set('profile', profile);
                 store.set('token', token);
                 $location.path("/");
                 $scope.loggedIn = auth.isAuthenticated;
-                console.log("end of authentication");
+                // Or using the object
+                $scope.message = 'message changed';
+                $scope.profile = auth.profile;
             }, function() {
                 // Error callback
-                debugger;
                 console.log("error with sign on");
             });
         }
@@ -32,6 +34,7 @@ sjModule.controller('loginController', ['$scope', 'auth', '$location', 'store',
             auth.signout();
             store.remove('profile');
             store.remove('token');
+            $scope.loggedIn = auth.isAuthenticated;
         }
     }
 ]);
