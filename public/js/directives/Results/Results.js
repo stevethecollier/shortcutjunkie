c1bc1cfb7a2c7fb9024d87528d5aedc89119eaa0
Juan Caicedo <juan.caicedo@alumni.ubc.ca>
2014-10-24 13:12:57 -0400

    title for shortcut home included

--- public/js/directives/Results/Results.js
+++ public/js/directives/Results/Results.js
@@ -1,6 +1,8 @@
 var sjModule = angular.module('sj');
 sjModule.directive('results', function(){
 	return {
+        restrict: 'E',
+        transclude: true,
 		templateUrl: '/js/directives/Results/results.html'
 	}
 });
