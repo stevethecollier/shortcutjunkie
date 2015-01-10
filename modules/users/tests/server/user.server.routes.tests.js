dfbaf35eb96755d7a35d9591cb4e0bd65cf043f6
Juan Caicedo <juan.caicedo@alumni.ubc.ca>
2015-01-10 18:51:55 -0500

    user test exits

---
+++ modules/users/tests/server/user.server.routes.tests.js
@@ -0,0 +1,72 @@
+'use strict';
+
+var should = require('should'),
+    request = require('supertest'),
+    path = require('path'),
+    mongoose = require('mongoose'),
+    User = mongoose.model('User'),
+    Shortcut = mongoose.model('Shortcut'),
+    express = require(path.resolve('./config/lib/express'));
+
+/**
+ * Globals
+ */
+var app, agent, credentials, user, shortcut;
+
+/**
+ * Shortcut routes tests
+ */
+describe('User route tests', function() {
+    before(function(done) {
+        // Get application
+        app = express.init(mongoose);
+        agent = request.agent(app);
+
+        done();
+    });
+
+    beforeEach(function(done) {
+        // Create user credentials
+        credentials = {
+            username: 'username',
+            password: 'password'
+        };
+
+        // Create a new user
+        user = new User({
+            firstName: 'Full',
+            lastName: 'Name',
+            displayName: 'Full Name',
+            email: 'test@test.com',
+            roles: ['user'],
+            username: credentials.username,
+            password: credentials.password,
+            provider: 'local'
+        });
+
+        // Save a user to the test db and create new Shortcut
+        user.save(function(error, user) {
+            shortcut = new Shortcut({
+                keyCombination: 'keyCombination',
+                application: 'application',
+                description: 'description',
+                operatingSystem: 'operatingSystem',
+                category: 'category',
+                created: Date.now(),
+                user: mongoose.Types.ObjectId(user._id)
+            });
+            shortcut.save(function(error, shortcut) {
+                done();
+            });
+        });
+    });
+
+    it('should run', function(done){
+        done();
+    })
+    afterEach(function(done) {
+        User.remove().exec();
+        Shortcut.remove().exec();
+        done();
+    });
+});
