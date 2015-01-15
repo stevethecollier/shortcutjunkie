8fd5a12fe6f33e3f810343ff594e214c9aa2a606
Juan Caicedo <juan.caicedo@alumni.ubc.ca>
2015-01-15 11:42:23 -0500

    shortcuts are being read

--- modules/shortcuts/server/bulk.upload/utils.js
+++ modules/shortcuts/server/bulk.upload/utils.js
@@ -2,7 +2,7 @@
 var fs = require('fs');
 
 exports.readFile = function(done) {
-    fs.readFile('modules/shortcuts/server/bulk.upload/photoshopPC.json', function(error, data) {
+    fs.readFile('modules/shortcuts/server/bulk.upload/photoshopPC.json', 'utf8', function(error, data) {
         if (!error && data){
             var shortcuts = JSON.parse(data);
             console.log(shortcuts);
