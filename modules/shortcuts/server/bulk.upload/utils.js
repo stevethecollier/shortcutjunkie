a5235ceeefb79f5794b291171c07ab7420f0cad4
Juan Caicedo <juan.caicedo@alumni.ubc.ca>
2015-01-15 16:27:48 -0500

    read, upload and delete jsons

--- modules/shortcuts/server/bulk.upload/utils.js
+++ modules/shortcuts/server/bulk.upload/utils.js
@@ -1,11 +1,16 @@
 'use strict';
 var fs = require('fs'),
     async = require('async'),
-    _ = require('lodash');
+    mongoose = require('mongoose'),
+    User = mongoose.model('User'),
+    Shortcut = mongoose.model('Shortcut'),
+    _ = require('lodash'),
+    logger = require('tracer').console();
+
+var shortcutsDir = 'modules/shortcuts/server/bulk.upload/shortcuts/';
+var files = fs.readdirSync(shortcutsDir);
 
 exports.readShortcuts = function(callback) {
-    var shortcutsDir = 'modules/shortcuts/server/bulk.upload/shortcuts/';
-    var files = fs.readdirSync(shortcutsDir);
 
     async.map(files,
         function(file, done) {
@@ -21,8 +26,40 @@ exports.readShortcuts = function(callback) {
             callback(_.flatten(shortcuts));
         }
     );
-}
+};
 
-exports.uploadShortcuts= function(shortcuts, callback){
-    callback(shortcuts);
-}
+exports.uploadShortcuts = function(shortcuts, callback) {
+    User.find({
+        username: 'editoruser'
+    }).exec(function(error, user) {
+        async.map(shortcuts,
+            function(shortcut, done) {
+                shortcut = _.extend(shortcut, {
+                    user: user._id
+                });
+                shortcut = new Shortcut(shortcut);
+                shortcut.save(function(error, shortcut) {
+                    if (error) done(error);
+                    else done(null, shortcut);
+                });
+            },
+            function(error, shortcuts) {
+                if (error) return console.log(error);
+                callback(null, shortcuts);
+            }
+        );
+    });
+};
+
+exports.deleteFiles = function(callback) {
+    async.map(files,
+        function(file, done) {
+            fs.unlink(shortcutsDir + file, function(error) {
+                return done(error);
+            });
+        },
+        function(error, shortcuts) {
+            callback(error);
+        }
+    );
+};
--- modules/shortcuts/tests/server/bulk.upload.utils.server.tests.js
+++ modules/shortcuts/tests/server/bulk.upload.utils.server.tests.js
@@ -4,28 +4,62 @@
  * Module dependencies.
  */
 var expect = require('chai').expect,
+    assert = require('chai').assert,
+    logger = require('tracer').console(),
     mongoose = require('mongoose'),
     User = mongoose.model('User'),
+    Shortcut = mongoose.model('Shortcut'),
     utils = require('../../server/bulk.upload/utils.js');
 
-var expected = [{
-    "keyCombination": "Option-Marquee",
-    "description": "Draw Marquee from Center",
-    "application": "Photoshop",
-    "operatingSystem": "OS X",
-    "category": "Selecting"
-}, {
-    "keyCombination": "Alt - Marquee",
-    "description": "Draw Marquee from Center",
-    "application": "Photoshop",
-    "operatingSystem": "Windows",
-    "category": "Selecting"
-}]
+
+/**
+ * Globals
+ */
+var user,
+    credentials,
+    expected = [{
+        "keyCombination": "Option-Marquee",
+        "description": "Draw Marquee from Center",
+        "application": "Photoshop",
+        "operatingSystem": "OS X",
+        "category": "Selecting"
+    }, {
+        "keyCombination": "Alt - Marquee",
+        "description": "Draw Marquee from Center",
+        "application": "Photoshop",
+        "operatingSystem": "Windows",
+        "category": "Selecting"
+    }]
 
 /**
  * Unit tests
  */
-describe('Bulk upload:', function() {
+describe('Bulk upload utils:', function() {
+    before(function(done) {
+        // Create user credentials
+        credentials = {
+            username: 'editoruser',
+            password: 'password'
+        };
+
+        // Create a new user
+        user = new User({
+            firstName: 'Full',
+            lastName: 'Name',
+            displayName: 'ShortcutJunkie',
+            email: 'test@test.com',
+            roles: ['user', 'editor'],
+            username: credentials.username,
+            password: credentials.password,
+            provider: 'local'
+        });
+        user.save(function(error, _user) {
+            user = _user;
+            done();
+        })
+    });
+
+
     it('reads shortcuts', function(done) {
         utils.readShortcuts(function(shortcuts) {
             expect(shortcuts).to.deep.equal(expected);
@@ -34,9 +68,31 @@ describe('Bulk upload:', function() {
     });
 
     it('saves shortcuts', function(done) {
-        utils.uploadShortcuts(expected, function(shortcuts){
-            expect(shortcuts).to.deep.equal(expected);
+        utils.uploadShortcuts(expected, function(error, saved) {
+            saved = JSON.stringify(saved);
+            saved = JSON.parse(saved);
+
+            saved.forEach(function(shortcut, index) {
+                expect(shortcut.application).to.equal(expected[index].application);
+                expect(shortcut.operatingSystem).to.equal(expected[index].operatingSystem);
+                expect(shortcut.category).to.equal(expected[index].category);
+                expect(shortcut.keyCombination).to.equal(expected[index].keyCombination);
+            });
+
             done();
         });
+    });
+
+    it('deletes the files', function(done){
+        utils.deleteFiles(function(error){
+            expect(error).to.not.exist;
+            done()
+        })
     })
+
+    after(function(done) {
+        User.remove().exec();
+        Shortcut.remove().exec();
+        done();
+    });
 });
