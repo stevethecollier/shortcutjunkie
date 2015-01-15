5b53e2b69e3cdf4da65cfa935f6b5bde5abccb8a
Juan Caicedo <juan.caicedo@alumni.ubc.ca>
2015-01-15 10:35:29 -0500

    scaffolding tests

---
+++ modules/shortcuts/tests/server/bulk.upload.utils.server.tests.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var expect = require('chai').expect,
+    mongoose = require('mongoose'),
+    User = mongoose.model('User'),
+    utils = require('../../server/bulk.upload/utils.js');
+
+/**
+ * Unit tests
+ */
+describe('Shortcut Model Unit Tests:', function() {
+    describe('Bulk upload', function() {
+        it('reads file', function(done) {
+            done()
+        });
+    });
+});
